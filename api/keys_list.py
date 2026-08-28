212c52b6656b80cdee6fe4276777dba54a166cb2
imluketheduke <duketopceo@gmail.com>
2026-08-27 18:59:40 -0600

    feat: initial openrouter_usage plugin v1.1.0

---
+++ api/keys_list.py
@@ -0,0 +1,12 @@
+from helpers.api import ApiHandler, Input, Output, Request
+from usr.plugins.openrouter_usage.helpers.openrouter_client import fetch_keys
+
+
+class KeysList(ApiHandler):
+    async def process(self, input: Input, request: Request) -> Output:
+        try:
+            context = self.use_context(str(input.get("context") or ""), create_if_not_exists=False)
+            agent = context.agent0 if context else None
+        except Exception:
+            agent = None
+        return fetch_keys(agent)
